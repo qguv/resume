b2b99de6190e07b526f12acd5a7463fe4bb04220
Quint Guvernator <quint@guvernator.net>
2016-02-22 15:20:50 -0500

    resume.sh: remove unnecessary sed call

--- targets/resume.sh
+++ targets/resume.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env sh
 
-# OS X is special
-uname="$(uname)"
-if [[ "$uname" == "Darwin" ]]; then
-	SED='sed -E'
-elif [[ "$uname" == "Linux" ]]; then
-	SED='sed -r'
-fi
-
 SOURCE="$1"
 DVIFILE="$SOURCE.dvi"
 PDFFILE="$SOURCE.pdf"
